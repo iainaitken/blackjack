0982524971f18b0be819e6357e5596af7503e184
iainaitken <iain_aitken@yahoo.co.uk>
2021-04-29 17:20:35 +0100

    Set up test environment

--- lib/card.rb
+++ lib/card.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
+# The Card class represents the cards with which the blackjack game will be played.
+# The class knows the suit and rank of the card, and also a label for gameplay purposes.
 class Card
   attr_reader :suit, :rank, :label
-  
+
   def initialize(suit:, rank:)
     @suit = suit
     @rank = rank
@@ -10,9 +14,9 @@ class Card
   private
 
   def create_label
-    label = "#{rank_shorthand}#{suit_shorthand}"
+    "#{rank_shorthand}#{suit_shorthand}"
   end
-  
+
   def rank_shorthand
     face_card? ? @rank.chr.upcase : @rank
   end
@@ -22,7 +26,7 @@ class Card
   end
 
   def face_card?
-    face_cards = ['ace', 'king', 'queen', 'jack']
+    face_cards = %w[ace king queen jack]
     face_cards.include?(@rank)
   end
 end
