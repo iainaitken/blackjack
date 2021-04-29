b260e2a84fa30702cee9dfd767f61265764a74b2
iainaitken <iain_aitken@yahoo.co.uk>
2021-04-29 16:38:38 +0100

    Refactor so that label works for any card

--- lib/card.rb
+++ lib/card.rb
@@ -1,12 +1,36 @@
 class Card
-  attr_reader :suit, :rank
+  attr_reader :suit, :rank, :label
   
   def initialize(suit:, rank:)
     @suit = suit
     @rank = rank
+    @label = create_label
   end
 
-  def label
-    "Ac"
+  private
+
+  def create_label
+    rank_shorthand = shorten_rank
+    suit_shorthand = shorten_suit
+    label = "#{rank_shorthand}#{suit_shorthand}"
+  end
+
+  def shorten_suit
+    @suit.chr
+  end
+
+  def shorten_rank
+    case @rank
+    when 'ace'
+      'A'
+    when 'king'
+      'K'
+    when 'queen'
+      'Q'
+    when 'jack'
+      'J'
+    else
+      @rank
+    end
   end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,22 +2,23 @@ require 'card'
 
 RSpec.describe Card do
   subject { described_class }
-  let(:card1) { subject.new(suit: "clubs", rank: "ace") }
-  let(:card2) { subject.new(suit: "spades", rank: "7") }
+  let(:card1) { subject.new(suit: 'clubs', rank: 'ace') }
+  let(:card2) { subject.new(suit: 'spades', rank: '7') }
 
   it 'has a suit and a rank' do
-    expect(card1.suit).to eq("clubs")
-    expect(card1.rank).to eq("ace")
+    expect(card1.suit).to eq('clubs')
+    expect(card1.rank).to eq('ace')
   end
 
   it 'works with number cards' do
-    expect(card2.suit).to eq("spades")
-    expect(card2.rank).to eq("7")
+    expect(card2.suit).to eq('spades')
+    expect(card2.rank).to eq('7')
   end
 
   describe '#label' do
     it 'displays the suit and rank as a label' do
-      expect(card1.label).to eq("Ac")
+      expect(card1.label).to eq('Ac')
+      expect(card2.label).to eq('7s')
     end
   end
 end
